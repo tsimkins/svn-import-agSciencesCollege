41354a95ccae7457b21ba28a87921ea3ce88f1f8
trs22 <trs22@ea9a3244-bef9-0310-a2a5-f2a3d2dd1ec8>
2009-02-18 13:58:50 +0000

    Added override for Penn State University in TitleViewlet. Some CSS changes. Updated thinkagain.swf logo

--- agSciencesCollege/agCommon/browser/viewlets.py
+++ agSciencesCollege/agCommon/browser/viewlets.py
@@ -3,6 +3,8 @@ from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from plone.app.layout.viewlets.common import ViewletBase
 from AccessControl import getSecurityManager
 from Products.CMFCore.utils import getToolByName
+from Products.CMFPlone.utils import safe_unicode
+from cgi import escape
 
 
 class TopNavigationViewlet(ViewletBase):   
@@ -12,3 +14,24 @@ class TopNavigationViewlet(ViewletBase):
 		context_state = getMultiAdapter((self.context, self.request),
                                         name=u'plone_context_state')
 		self.topnavigation = context_state.actions().get('topnavigation', None)
+
+
+class TitleViewlet(ViewletBase):
+	
+	def update(self):
+		self.portal_state = getMultiAdapter((self.context, self.request),
+											name=u'plone_portal_state')
+		self.context_state = getMultiAdapter((self.context, self.request),
+											 name=u'plone_context_state')
+		self.page_title = self.context_state.object_title
+		self.portal_title = self.portal_state.portal_title
+		
+	def index(self):
+		portal_title = safe_unicode(self.portal_title())
+		page_title = safe_unicode(self.page_title())
+		if page_title == portal_title:
+			return u"<title>%s &mdash; Penn State University</title>" % (escape(portal_title))
+		else:
+			return u"<title>%s &mdash; %s &mdash; Penn State University</title>" % (
+				escape(safe_unicode(page_title)),
+				escape(safe_unicode(portal_title)))
