de3169bf773e35bf741b750d3d622dd48421a5e9
trs22 <trs22@ea9a3244-bef9-0310-a2a5-f2a3d2dd1ec8>
2009-03-22 23:00:32 +0000

    Adding getHomepageImage.py, which should be placed in any background-images, and renamed to .js

---
+++ agSciencesCollege/agCommon/skins/agcommon_custom_templates/getHomepageImage.py
@@ -0,0 +1,51 @@
+from random import random
+
+backgrounds = []
+backgroundAlignments = []
+mySWF = ""
+
+for myImage in context.listFolderContents(contentFilter={"portal_type" : "Image"}):
+    backgrounds.append(myImage.absolute_url())
+
+    if myImage.hasProperty("align"):
+        backgroundAlignments.append(myImage.align)
+    else:
+        backgroundAlignments.append("center")
+
+
+for myFile in context.listFolderContents():
+
+    if myFile.absolute_url().endswith(".swf"):
+
+        mySWF = myFile.absolute_url()
+
+
+print """
+var bodyClass = document.body.className;
+
+if(bodyClass.match(/template-document_homepage_view/))
+{
+	var homepageImage = document.getElementById("homepageimage");
+
+	if (homepageImage)
+	{
+		var backgrounds = "%s".split(";");
+		var backgroundAlignments = "%s".split(";");
+		var randomnumber = Math.floor(Math.random()*backgrounds.length) ;
+		homepageImage.style.backgroundImage = "url(" + backgrounds[randomnumber] + ")";
+		homepageImage.style.backgroundPosition = backgroundAlignments[randomnumber];
+
+		var so = new SWFObject("background-images/%s", "thinkAgain", "585", "265", "8", "transparent");
+		so.addParam("wmode","transparent");
+		so.write("homepageimage");
+	}
+
+}
+
+""" % (";".join(backgrounds), ";".join(backgroundAlignments), mySWF)
+
+
+
+
+return printed
+
