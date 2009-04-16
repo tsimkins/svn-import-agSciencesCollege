f4043c24790d19794393326f7e694d68c66ea58e
trs22 <trs22@ea9a3244-bef9-0310-a2a5-f2a3d2dd1ec8>
2009-04-16 15:13:36 +0000

    This sets what modules can be used by Python scripts.

---
+++ agSciencesCollege/GlobalModules/__init__.py
@@ -0,0 +1,7 @@
+from Products.PythonScripts.Utility import allow_module
+allow_module('feedparser')
+allow_module('datetime')
+allow_module('datetime.datetime')
+allow_module('Products.feedSync')
+allow_module('Products.feedSync.sync')
+
