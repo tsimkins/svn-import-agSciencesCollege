0e2bad3b42677472e7770a9e110cf52398c3222e
trs22 <trs22@ea9a3244-bef9-0310-a2a5-f2a3d2dd1ec8>
2009-05-06 19:32:20 +0000

    Removed the import_steps.xml and setuphandlers method of doing an uninstall, and substituted Extensions/Install.py

--- agSciencesCollege/FolderTextAndImage/Extensions/Install.py
+++ agSciencesCollege/FolderTextAndImage/Extensions/Install.py
@@ -1,18 +1,16 @@
 from zope.component import getSiteManager
 from Products.FolderTextAndImage.extender import FolderTextAndImage
 
-def install(context):
+def install(portal):
     """Register the extender so it takes effect on this Plone site."""
-    portal = context.getSite()
     sm = getSiteManager(portal)
     # Local components are not per-container; they are per-sitemanager. It just so happens that every Plone site has a sitemanager. Hooray.
     sm.registerAdapter(FolderTextAndImage, name='FolderTextAndImage')
     
     return "Registered the extender at the root of the Plone site."
 
-def uninstall(context):
+def uninstall(portal):
     """Unregister the schema extender so it no longer takes effect on this Plone site."""
-    portal = context.getSite()
     sm = getSiteManager(portal)
     sm.unregisterAdapter(FolderTextAndImage, name='FolderTextAndImage')
     
