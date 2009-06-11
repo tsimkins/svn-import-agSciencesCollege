00ad2bc5fab759b8b1f7be637349db53f6f64653
trs22 <trs22@ea9a3244-bef9-0310-a2a5-f2a3d2dd1ec8>
2009-06-11 18:25:38 +0000

    Added modules for ploneimport to work.

--- agSciencesCollege/GlobalModules/__init__.py
+++ agSciencesCollege/GlobalModules/__init__.py
@@ -4,4 +4,8 @@ allow_module('datetime')
 allow_module('datetime.datetime')
 allow_module('Products.feedSync')
 allow_module('Products.feedSync.sync')
+allow_module('Products.CMFCore.utils')
+allow_module('Products.CMFCore.utils.getToolByName')
+allow_module('zope.component')
+allow_module('zope.component.getSiteManager')
 
