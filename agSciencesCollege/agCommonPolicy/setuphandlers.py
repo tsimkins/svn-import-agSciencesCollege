4dad0d0ca31230cae53e2547eb1e6a2d82125433
trs22 <trs22@ea9a3244-bef9-0310-a2a5-f2a3d2dd1ec8>
2009-07-07 15:32:31 +0000

    Updated sitemap.xml.gz and inline editing preferences. Fixed portlets, mimetypes, and folders.

--- agSciencesCollege/agCommonPolicy/setuphandlers.py
+++ agSciencesCollege/agCommonPolicy/setuphandlers.py
@@ -9,8 +9,10 @@
 # created 2006-11-03 by Tom Lazar <tom@tomster.org>, http://tomster.org/
 # under a BSD-style licence (i.e. use as you wish but don't sue me)
 
-from zope.component import getSiteManager
+from zope.component import getSiteManager, getUtility, getMultiAdapter
+from zope.component.interfaces import ComponentLookupError
 from Products.CMFCore.utils import getToolByName
+from plone.portlets.interfaces import IPortletManager, IPortletAssignmentMapping
 import string
 import random
 
@@ -83,11 +85,15 @@ def randomPassword():
 def deleteUnusedFolders(context):
 	site = context.getSite()
 	for theFolder in ['Members', 'news', 'events']:
+
 		if theFolder in site.objectIds():
-			urltool = getToolByName(site, "portal_url")
-			portal = urltool.getPortalObject()
-			portal.manage_delObjects([theFolder])
-			print "deleted folder %s" % theFolder
+
+			theFolderObject = getattr(site, theFolder)
+			if theFolderObject.getPortalTypeName() != 'Folder':			
+				urltool = getToolByName(site, "portal_url")
+				portal = urltool.getPortalObject()
+				portal.manage_delObjects([theFolder])
+				print "deleted folder %s" % theFolder
 		else:
 			print "Folder %s not found" % theFolder
 			
@@ -97,7 +103,7 @@ def createSiteFolders(context):
 	# Publish from http://svn.cosl.usu.edu/svndev/eduCommons3/branches/yale-3.0.2/setupHandlers.py
 	wftool =  getToolByName(site, 'portal_workflow')
 
-	for theArray in [['about', 'About Us'], ['news', 'News'], ['events', 'Events'], ['contact', 'Contact Us']]:
+	for theArray in [['about', 'About Us'], ['news', 'News'], ['events', 'Events'], ['contact', 'Contact Us'], ['background-images', 'Background Images']]:
 		(theId, theTitle) = theArray
 		
 		if theId not in site.objectIds():
@@ -107,6 +113,10 @@ def createSiteFolders(context):
 			
 			if wftool.getInfoFor(theObject, 'review_state') != 'Published':
 				wftool.doActionFor(theObject, 'publish')
+
+			if theId == 'background-images':
+				theObject.setExcludeFromNav(True)
+				theObject.reindexObject()
 
 			print "Created and published folder %s" % theId
 		else:
@@ -121,29 +131,95 @@ def configureFrontPage(context):
 		frontPage.setTitle('Home')
 		frontPage.setDescription('')
 		frontPage.setPresentation(False)
+		frontPage.setLayout("document_homepage_view")
+
 		
 def setSitePortlets(context):
 
 	site = context.getSite()
-	
-	leftColumn = getUtility(IPortletManager, name=u'plone.leftcolumn', context=site)
-	left = getMultiAdapter((portal, leftColumn), IPortletAssignmentMapping, context=site)
-	
-	rightColumn = getUtility(IPortletManager, name=u'plone.rightcolumn', context=portal)
-	right = getMultiAdapter((portal, rightColumn), IPortletAssignmentMapping, context=portal)
 
-	# Turn off right hand portlets
-	if u'review' in right:
-		del right[u'review']
+	print "Setting site portlets."
+
+	try:
+
+		ploneLeftColumn = getUtility(IPortletManager, name=u'plone.leftcolumn', context=site)
+		ploneLeft = getMultiAdapter((site, ploneLeftColumn), IPortletAssignmentMapping, context=site)
+
+		ploneRightColumn = getUtility(IPortletManager, name=u'plone.rightcolumn', context=site)
+		ploneRight = getMultiAdapter((site, ploneRightColumn), IPortletAssignmentMapping, context=site)
 	
-	if u'news' in right:
-		del right[u'news']
+	except ComponentLookupError:
+		print "ComponentLookupError"
+
+	else:
+
+		for deletePortlet in [u'review', u'news', u'events', u'calendar']:
+			try:
+				del ploneRight[deletePortlet]
+				print "Deleted %s" % deletePortlet
+			except KeyError:
+				print "No portlet named %s" % deletePortlet
+			
+			
+		for deletePortlet in [u'login']:
+			try:
+				del ploneLeft[deletePortlet]
+				print "Deleted %s" % deletePortlet
+			except KeyError:
+				print "No portlet named %s" % deletePortlet
+
+		if ploneLeft.has_key('navigation'):
+			print "Set navigation portlet start level"
+			ploneLeft['navigation'].topLevel = 0
+
+
+def addExtensionToMimeType(registry, extension, mimetype):
+	for myMime in registry.lookup(mimetype):
+		glob = '*.%s' % extension
+		registry.register_extension(extension, myMime)
+		registry.register_glob(glob, myMime)
+
+		newExtensions = list(myMime.extensions)
+		newExtensions.append(extension)
+		newExtensions = tuple(newExtensions)
+
+		newGlobs = list(myMime.globs)
+		newGlobs.append(glob)
+		newGlobs = tuple(newGlobs)
+
+		myMime.edit(myMime.name(), myMime.mimetypes, newExtensions, myMime.icon_path, globs=newGlobs)
+
+		print "Added extension %s to mimetype %s" % (extension, mimetype)
+
+def configureMimeTypes(context):
 	
-	if u'events' in right:
-		del right[u'events']
+	site = context.getSite()
 	
-	if u'calendar' in right:
-		del right[u'calendar']
+	listOfMimeTypes = [
+			['docm', 'application/msword'],
+			['docx', 'application/msword'],
+			['dotm', 'application/msword'],
+			['dotx', 'application/msword'],
+			['potm', 'application/vnd.ms-powerpoint'],
+			['potx', 'application/vnd.ms-powerpoint'],
+			['ppam', 'application/vnd.ms-powerpoint'],
+			['ppsm', 'application/vnd.ms-powerpoint'],
+			['ppsx', 'application/vnd.ms-powerpoint'],
+			['pptm', 'application/vnd.ms-powerpoint'],
+			['pptx', 'application/vnd.ms-powerpoint'],
+			['xlam', 'application/vnd.ms-excel'],
+			['xlsb', 'application/vnd.ms-excel'],
+			['xlsm', 'application/vnd.ms-excel'],
+			['xlsx', 'application/vnd.ms-excel'],
+			['xltm', 'application/vnd.ms-excel'],
+			['xltx', 'application/vnd.ms-excel']
+	]
+
+	mimetypes_registry = getattr(site, 'mimetypes_registry')
+
+	for (extension, mimetype) in listOfMimeTypes:
+		 addExtensionToMimeType(mimetypes_registry, extension, mimetype)
+
 
 def setupHandlersWrapper(context):
 	
@@ -156,9 +232,10 @@ def setupHandlersWrapper(context):
 	createSiteFolders(context)
 
 	configureFrontPage(context)
-
 	
-			
+	setSitePortlets(context)
+
+	configureMimeTypes(context)
 
 	
 	
