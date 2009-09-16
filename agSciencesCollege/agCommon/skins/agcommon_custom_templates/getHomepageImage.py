7301f45cccb4b0289543029913f20ead8dfb4cb0
trs22 <trs22@ea9a3244-bef9-0310-a2a5-f2a3d2dd1ec8>
2009-09-16 20:28:09 +0000

    Updated getHomepageImage.py to check for image height and set the homepageimage div height appropriately.

--- agSciencesCollege/agCommon/skins/agcommon_custom_templates/getHomepageImage.py
+++ agSciencesCollege/agCommon/skins/agcommon_custom_templates/getHomepageImage.py
@@ -1,6 +1,6 @@
 backgrounds = []
 backgroundAlignments = []
-mySWF = ""
+backgroundHeights = []
 
 for myImage in context.listFolderContents(contentFilter={"portal_type" : "Image"}):
     backgrounds.append(myImage.absolute_url())
@@ -10,14 +10,8 @@ for myImage in context.listFolderContents(contentFilter={"portal_type" : "Image"
     else:
         backgroundAlignments.append("center")
 
-
-for myFile in context.listFolderContents():
-
-    if myFile.absolute_url().endswith(".swf"):
-
-        mySWF = myFile.absolute_url()
-
-
+    backgroundHeights.append(str(myImage.getHeight()))
+    
 print """
 var bodyClass = document.body.className;
 
@@ -29,17 +23,16 @@ if(bodyClass.match(/template-document_homepage_view/))
 	{
 		var backgrounds = "%s".split(";");
 		var backgroundAlignments = "%s".split(";");
+		var backgroundHeights = "%s".split(";");
 		var randomnumber = Math.floor(Math.random()*backgrounds.length) ;
 		homepageImage.style.backgroundImage = "url(" + backgrounds[randomnumber] + ")";
 		homepageImage.style.backgroundPosition = backgroundAlignments[randomnumber];
-
-		var so = new SWFObject("%s", "thinkAgain", "585", "265", "8", "transparent");
-		so.addParam("wmode","transparent");
-		so.write("homepageimage");
+		homepageImage.style.height = backgroundHeights[randomnumber] + 'px';
 	}
 
 }
 
-""" % (";".join(backgrounds), ";".join(backgroundAlignments), mySWF)
+""" % (";".join(backgrounds), ";".join(backgroundAlignments), ";".join(backgroundHeights))
 
 return printed
+
