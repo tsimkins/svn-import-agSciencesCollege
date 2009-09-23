843c9f03dd44e20bb2bf71c13a30b54f7a5c92bb
trs22 <trs22@ea9a3244-bef9-0310-a2a5-f2a3d2dd1ec8>
2009-09-23 17:50:03 +0000

    RC2 for agCommon 2.0
     * Added dynamic gradient functionality.
     * addthis fixes
     * portlet/viewlet fixes
    
    TODO:
     * Homepage Flash
     * Footer link

--- agSciencesCollege/agCommon/__init__.py
+++ agSciencesCollege/agCommon/__init__.py
@@ -1,6 +1,31 @@
 # Register our skins directory - this makes it available via portal_skins.
 
 from Products.CMFCore.DirectoryView import registerDirectory
+from subprocess import Popen,PIPE
+import re
 
 GLOBALS = globals()
 registerDirectory('skins', GLOBALS)
+
+def gradientBackground(request):
+    
+    startColor = request.form.get("startColor", '000000')
+    endColor = request.form.get("endColor", 'FFFFFF')
+    
+    try:
+        height = str(int(request.form.get("height", '600')))
+    except ValueError:
+        height = '600';
+    
+    # Validate we have a color code
+    colorRegex = "^[0-9A-Fa-f]{3,6}$"
+    
+    if not re.match(colorRegex, startColor):
+        startColor = '000000'
+    
+    if not re.match(colorRegex, endColor):
+        endColor = 'FFFFFF'
+    
+    png = Popen(['convert', '-size', '8x%s'%height, 'gradient:#%s-#%s'%(str(startColor), str(endColor)), 'png:-'], stdout=PIPE)
+
+    return "".join(png.stdout.readlines())
