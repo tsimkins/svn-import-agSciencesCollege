65089a699e7d88cc12fce0214726848807124996
trs22 <trs22@ea9a3244-bef9-0310-a2a5-f2a3d2dd1ec8>
2009-10-07 13:12:25 +0000

    Added fixPhoneNumber function to convert (xxx) xxx-xxxx to xxx-xxx-xxxx

--- agSciencesCollege/GlobalModules/__init__.py
+++ agSciencesCollege/GlobalModules/__init__.py
@@ -1,9 +1,13 @@
 from Products.PythonScripts.Utility import allow_module
+import re
+
 allow_module('feedparser')
 allow_module('datetime')
 allow_module('datetime.datetime')
 allow_module('Products.feedSync')
 allow_module('Products.feedSync.sync')
+allow_module('Products.feedSync.cvent')
+allow_module('Products.feedSync.cvent.importEvents')
 allow_module('Products.CMFCore.utils')
 allow_module('Products.CMFCore.utils.getToolByName')
 allow_module('zope.component')
@@ -12,12 +16,19 @@ allow_module('zope.component.getSiteManager')
 allow_module('Products.GlobalModules')
 allow_module('Products.GlobalModules.makeHomePage')
 allow_module('Products.GlobalModules.makePhotoFolder')
+allow_module('Products.GlobalModules.fixPhoneNumber')
 
 allow_module('Products.agCommon')
 allow_module('Products.agCommon.gradientBackground')
 
 allow_module('ZODB.POSException')
 allow_module('ZODB.POSException.POSKeyError')
+
+allow_module('re')
+allow_module('re.sub')
+allow_module('re.compile')
+
+phoneRegex = re.compile(r"^\((\d{3})\)\s+(\d{3})\-(\d{4})$")
 
 def makeHomePage(context):
     print context.portal_type
@@ -38,3 +49,16 @@ def makePhotoFolder(context):
     print context.portal_type
     print context.archetype_name
     print "OK"
+
+def fixPhoneNumber(myPerson):
+
+    phone = myPerson.getOfficePhone()
+    newPhone = phoneRegex.sub(r"\1-\2-\3", phone)
+
+    if newPhone != phone:
+        myPerson.setOfficePhone(newPhone)
+        myPerson.reindexObject()
+        return "%s: from %s to %s" % (myPerson.id, phone, newPhone)
+    else:
+        return "%s: Phone OK" % myPerson.id
+
