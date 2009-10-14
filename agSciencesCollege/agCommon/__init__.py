8074fb3ed4eea976d799136092a53a392980c772
trs22 <trs22@ea9a3244-bef9-0310-a2a5-f2a3d2dd1ec8>
2009-10-14 20:24:47 +0000

    * Added placeholder image  * Put getHomepageImage and gradientBackground logic in __init__.py and made these scripts just wrapper scripts.  The agCommonPolicy product will 'copy' them to the site root and set the caching.  * Fixed up some base_properties

--- agSciencesCollege/agCommon/__init__.py
+++ agSciencesCollege/agCommon/__init__.py
@@ -11,7 +11,8 @@ registerDirectory('skins', GLOBALS)
 # Allow us to use this module in scripts
 
 allow_module('Products.agCommon')
-allow_module('Products.agCommon.gradientBackground')
+
+# Given start and end colors (optionally width and height) returns a gradient png
 
 def gradientBackground(request):
     
@@ -40,3 +41,55 @@ def gradientBackground(request):
     png = Popen(['convert', '-size', '%sx%s'%(width, height), 'gradient:#%s-#%s'%(str(startColor), str(endColor)), 'png:-'], stdout=PIPE)
 
     return "".join(png.stdout.readlines())
+
+
+# Given a context, gets a list of all images and returns a JavaScript snippet
+# that randomly picks one of them.
+#
+# To set an alignment (left, right) set a property of "align" on the image object.
+# Otherwise, it defaults to center.
+
+def getHomepageImage(context):
+    backgrounds = []
+    backgroundAlignments = []
+    backgroundHeights = []
+    
+    for myImage in context.listFolderContents(contentFilter={"portal_type" : "Image"}):
+        backgrounds.append(myImage.absolute_url())
+    
+        if myImage.hasProperty("align"):
+            backgroundAlignments.append(myImage.align)
+        else:
+            backgroundAlignments.append("center")
+    
+        backgroundHeights.append(str(myImage.getHeight()))
+        
+    
+    if not len(backgrounds):
+        backgrounds = ['homepage_placeholder.jpg']
+        backgroundAlignments = ['left']
+        backgroundHeights = ['265']
+    
+    return """
+    var bodyClass = document.body.className;
+    
+    if(bodyClass.match(/template-document_homepage_view/))
+    {
+        var homepageImage = document.getElementById("homepageimage");
+    
+        if (homepageImage)
+        {
+            var backgrounds = "%s".split(";");
+            var backgroundAlignments = "%s".split(";");
+            var backgroundHeights = "%s".split(";");
+            var randomnumber = Math.floor(Math.random()*backgrounds.length) ;
+            homepageImage.style.backgroundImage = "url(" + backgrounds[randomnumber] + ")";
+            homepageImage.style.backgroundPosition = backgroundAlignments[randomnumber];
+            homepageImage.style.height = backgroundHeights[randomnumber] + 'px';
+        }
+    
+    }
+    
+    """ % (";".join(backgrounds), ";".join(backgroundAlignments), ";".join(backgroundHeights))
+
+
--- agSciencesCollege/agCommon/skins/agcommon_templates/getHomepageImage.py
+++ agSciencesCollege/agCommon/skins/agcommon_templates/getHomepageImage.py
@@ -1,38 +1,4 @@
-backgrounds = []
-backgroundAlignments = []
-backgroundHeights = []
+from Products.agCommon import getHomepageImage
 
-for myImage in context.listFolderContents(contentFilter={"portal_type" : "Image"}):
-    backgrounds.append(myImage.absolute_url())
-
-    if myImage.hasProperty("align"):
-        backgroundAlignments.append(myImage.align)
-    else:
-        backgroundAlignments.append("center")
-
-    backgroundHeights.append(str(myImage.getHeight()))
-    
-print """
-var bodyClass = document.body.className;
-
-if(bodyClass.match(/template-document_homepage_view/))
-{
-	var homepageImage = document.getElementById("homepageimage");
-
-	if (homepageImage)
-	{
-		var backgrounds = "%s".split(";");
-		var backgroundAlignments = "%s".split(";");
-		var backgroundHeights = "%s".split(";");
-		var randomnumber = Math.floor(Math.random()*backgrounds.length) ;
-		homepageImage.style.backgroundImage = "url(" + backgrounds[randomnumber] + ")";
-		homepageImage.style.backgroundPosition = backgroundAlignments[randomnumber];
-		homepageImage.style.height = backgroundHeights[randomnumber] + 'px';
-	}
-
-}
-
-""" % (";".join(backgrounds), ";".join(backgroundAlignments), ";".join(backgroundHeights))
-
+print getHomepageImage(context)
 return printed
-
