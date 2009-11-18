bef95966a63f6962b0c679cb0e3eff48df37316f
trs22 <trs22@ea9a3244-bef9-0310-a2a5-f2a3d2dd1ec8>
2009-11-18 13:42:46 +0000

    Added tagmanager into product.

---
+++ agSciencesCollege/agCommon/branches/dev/skins/agcommon_javascript/tagmanager.js
@@ -0,0 +1,91 @@
+/* tagmanager.js */
+
+    function manageTags(prefix, label)
+    {
+        existingTags = jq('#subject_existing_keywords option');
+
+        for (var i=0; i<existingTags.size(); i++)
+        {
+            tag = existingTags.eq(i);
+            
+            if (tag.val().substring(0,prefix.length+1) == prefix + "-")
+            {
+                tag.hide();
+            }
+        }
+
+        mainTagLink = jq('<a href="#" id="edit_' + prefix + '_tags">' + label + '</a>');
+
+        mainTagLink.click(
+            function() 
+            { 
+                modalTags = jq('<div id="modal_tags"><h1>Choose the tags for this item:</h1></div>');
+                
+                existingTags = jq('#subject_existing_keywords option');
+
+                for (var i=0; i<existingTags.size(); i++)
+                {
+                    tag = existingTags.eq(i);
+                    
+                    if (tag.val().substring(0,5) == 'main-')
+                    {
+                        tagLabel = tag.val().substring(5,tag.val().length)
+                        checkbox = jq('<input name="extension_tag" type="checkbox" value="' + tag.val() + '" /> <span>' + tagLabel + '</span><br />')
+    
+                        if (tag.attr("selected"))
+                        {
+                            checkbox.attr("checked", "checked");
+                        }
+
+                        checkbox.appendTo(modalTags);
+                    }
+                }
+                
+                buttons = jq('<div id="modal_buttons"></div>');
+                jq('<input type="button" value="Cancel" class="simplemodal-close" /> ').appendTo(buttons);
+                submit = jq('<input type="button" value="Submit" class="simplemodal-close" />');
+                submit.click(
+                    function () {
+//                        debug = jq('#modal_tags');
+                        newTags = jq('#modal_tags input');
+                        existingTags = jq('#subject_existing_keywords option');
+                        for (var i=0; i<=newTags.size(); i++)
+                        {
+                            newTag = newTags.eq(i);
+
+                            if (newTag.attr("type") == "checkbox")
+                            {
+                                for (var j=0; j<existingTags.size(); j++)
+                                {
+                                    oldTag = existingTags.eq(j);
+//                                    /jq('<div>' + oldTag.val() + ' ? ' + newTag.val() + '</div>').appendTo(debug);
+
+                                    if (oldTag.val() == newTag.val())
+                                    {
+                                        oldTag.attr("selected", newTag.attr("checked"));
+                                    }
+                                }
+                            }
+
+                        }
+                        //alert("pause");
+                    }
+
+                );
+                submit.appendTo(buttons);
+                buttons.appendTo(modalTags);
+                
+                modalTags.modal(); 
+                return false;
+            }
+        )
+        
+        mainTagLink.appendTo('#archetypes-fieldname-subject');
+
+    }
+
+
+jq(document).ready(
+    manageTags("county", "Edit County Tags");
+    manageTags("main", "Edit Topic Tags");
+)
