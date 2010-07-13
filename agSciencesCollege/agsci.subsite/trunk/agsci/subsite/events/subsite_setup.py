f318d02f1fa198c81be045ff30880fc1ae6d37a3
trs22 <trs22@ea9a3244-bef9-0310-a2a5-f2a3d2dd1ec8>
2010-07-13 19:58:44 +0000

    Got subsite features working.

--- agSciencesCollege/agsci.subsite/trunk/agsci/subsite/events/subsite_setup.py
+++ agSciencesCollege/agsci.subsite/trunk/agsci/subsite/events/subsite_setup.py
@@ -3,29 +3,64 @@ from Products.CMFCore.utils import getToolByName
 from zope.component import getUtility
 from zope.component import getMultiAdapter
 from zope.app.container.interfaces import INameChooser
+import pdb
 
-from plone.portlets.interfaces import IPortletManager
-from plone.portlets.interfaces import IPortletAssignmentMapping, ILocalPortletAssignmentManager
-from plone.portlet.collection.collection import Assignment
+from plone.portlets.interfaces import IPortletManager, IPortletAssignmentMapping, ILocalPortletAssignmentManager
 
 from plone.portlets.constants import CONTEXT_CATEGORY
 
 from plone.app.portlets.portlets import navigation
+from collective.portlet import feedmixer
+from plone.portlet.collection import collection
 
 from datetime import datetime
 
+from zLOG import LOG, INFO
+
+from zope.component.interfaces import ComponentLookupError
+
+# wrapper functions for working with portlets. All the adaptors kept confusing me.
+def getPortletAssignmentMapping(context, name):
+    portlet_manager = getPortletManager(context, name)
+    return getMultiAdapter((context, portlet_manager), IPortletAssignmentMapping, context=context)
+
+def getPortletManager(context, name):
+    return getUtility(IPortletManager, name=name, context=context)
+
+def getLocalPortletAssignmentManager(context, name):
+    portlet_manager = getPortletManager(context, name)
+    return getMultiAdapter((context, portlet_manager), ILocalPortletAssignmentManager)
+    
+# Save the portlet assignment
+def saveAssignment(mapping, assignment):
+    chooser = INameChooser(mapping)
+    mapping[chooser.chooseName(None, assignment)] = assignment
+
+# Write debug messages to log file
+def writeDebug(msg):
+    LOG('agsci.subsite', INFO, msg)
+    
 # What we want to happen when we create a subsite
 def onSubsiteCreation(subsite, event):
 
+    writeDebug('Beginning post create script.')
+
+    # Calculate dates
     now = datetime.now()
     current_year = now.year
 
+    # Get URL tool
+    urltool = getToolByName(subsite, 'portal_url')
+    
     # Remove top menu
+    writeDebug('Removing top menu')
     subsite.manage_addProperty('top-menu', 'none', 'string')
+
+    writeDebug('Creating news folder')
     
     # Create News folder
     if 'news' not in subsite.objectIds():
-        subsite.invokeFactory('Folder', id='news', title='News')
+        subsite.invokeFactory(type_name='Folder', id='news', title='News')
         obj = subsite['news']
         obj.setConstrainTypesMode(1) # restrict what this folder can contain
         obj.setImmediatelyAddableTypes([])
@@ -33,13 +68,13 @@ def onSubsiteCreation(subsite, event):
         obj.reindexObject()
         
         # Create year folders for the past year, and the next 10 years
-        archive_years = range(current_year-1,current_year+11)
+        archive_years = [str(x) for x in range(current_year-1,current_year+11)]
 
         for year in archive_years:
-            year_string = str(year)
-            if year_string not in obj.objectIds():
-                obj.invokeFactory('Folder', id=year_string, title=year_string)
-                archive_folder = obj[year_string]
+            if year not in obj.objectIds():
+                writeDebug('Creating archive year folder %s' % year)
+                obj.invokeFactory(type_name='Folder', id=year, title=year)
+                archive_folder = obj[year]
                 archive_folder.setLayout('news_listing')
                 archive_folder.setExcludeFromNav(True)
                 archive_folder.setConstrainTypesMode(1) # restrict what this folder can contain
@@ -47,9 +82,19 @@ def onSubsiteCreation(subsite, event):
                 archive_folder.setLocallyAllowedTypes(['Link','News Item'])
                 archive_folder.reindexObject()
 
+        # Create sample news item
+        writeDebug('Creating sample news item')
+        if str(current_year) in obj.objectIds():
+            current_year_folder = obj[str(current_year)]
+            current_year_folder.invokeFactory(type_name='News Item', id='sample', title='Sample News Item', description='This is a sample News Item', text='<p>You may delete this item</p>', effective_date="%s-01-01" % str(current_year))
+        else:
+            writeDebug('WTF? %s' % str(obj.objectIds()))            
+
+        writeDebug('Creating latest news collection')
+
         # create a smartfolder for latest news items, and set it as the default page
         if 'latest' not in obj.objectIds():
-            obj.invokeFactory('Topic', id='latest', title='Latest News')
+            obj.invokeFactory(type_name='Topic', id='latest', title='Latest News')
             obj.setDefaultPage('latest')
             
             smart_obj = obj['latest']
@@ -63,14 +108,18 @@ def onSubsiteCreation(subsite, event):
             path_crit.setValue(obj.UID()) # Only list news in the news folder
             path_crit.setRecurse(True)
 
-            sort_crit = smart_obj.addCriterion('EffectiveDate','ATSortCriterion')
+            sort_crit = smart_obj.addCriterion('effective','ATSortCriterion')
             sort_crit.setReversed(True)
+
+        writeDebug('Creating years collection')
 
         # create a smartfolder for archived news by year inside the folder
         if 'years' not in obj.objectIds():
-            obj.invokeFactory('Topic', id='years', title='Archive')
+            obj.invokeFactory(type_name='Topic', id='years', title='Archive')
             
             smart_obj = obj['years']
+            smart_obj.setExcludeFromNav(True)
+            smart_obj.reindexObject()
         
             # Set the criteria for the folder
             type_crit = smart_obj.addCriterion('Type','ATPortalTypeCriterion')
@@ -87,46 +136,76 @@ def onSubsiteCreation(subsite, event):
             sort_crit = smart_obj.addCriterion('getId','ATSortCriterion')
             sort_crit.setReversed(True)
 
+
+        # Add News Archive Portlet to right column
+        writeDebug('Adding News Archive Portlet to right column')
+        news_RightColumn = getPortletAssignmentMapping(obj, 'plone.rightcolumn')
+        archiveCollectionPortlet = collection.Assignment(header=u"Archive",
+                                        target_collection = '/'.join(urltool.getRelativeContentPath(subsite.news.years)),
+                                        random=False,
+                                        show_more=False,
+                                        show_dates=False)
+
+        saveAssignment(news_RightColumn, archiveCollectionPortlet)
+
+
+        writeDebug('Creating spotlight folder')
+
         # create a folder for spotlight items inside the news folder
         if 'spotlight' not in obj.objectIds():
-            obj.invokeFactory('Folder', id='spotlight', title='Spotlight')
+            obj.invokeFactory(type_name='Folder', id='spotlight', title='Spotlight')
             
             spotlight_obj = obj['spotlight']
+            spotlight_obj.setConstrainTypesMode(1) # restrict what this folder can contain
+            spotlight_obj.setImmediatelyAddableTypes(['Link','Folder','File','Document'])
+            spotlight_obj.setLocallyAllowedTypes(['Link','Folder','File','Document','Topic'])
+            spotlight_obj.reindexObject()
             
-            spotlight_obj.invokeFactory('Topic', id='recent', title='Spotlight')
+            writeDebug('Creating recent spotlight topic')
+            spotlight_obj.invokeFactory(type_name='Topic', id='recent', title='Spotlight')
             
             smart_obj = spotlight_obj['recent']
+            
+            spotlight_obj.setDefaultPage('recent')
         
             # Set the criteria for the folder
-            type_crit = smart_obj.addCriterion('Type','ATPortalTypeCriterion')
-            
-            type_crit.setValue(['Folder']) # only our specified event types
-            
             path_crit = smart_obj.addCriterion('path','ATPathCriterion')
             path_crit.setValue(subsite.UID()) # Only list items in the current subsite
             path_crit.setRecurse(True)
 
-            path_crit = smart_obj.addCriterion('getId','ATListCriterion')
-            path_crit.setValue(archive_years)
+            # Set the criteria for the folder
+            tag_crit = smart_obj.addCriterion('Subject','ATSelectionCriterion')
+            tag_crit.setValue("spotlight-%s" % subsite.id) # Only list items in the current subsite
         
-            sort_crit = smart_obj.addCriterion('getId','ATSortCriterion')
+            sort_crit = smart_obj.addCriterion('sortable_title','ATSortCriterion')
             sort_crit.setReversed(True)
+            
+            # Create sample spotlight item
+            writeDebug('Creating sample spotlight item')
+            spotlight_obj.invokeFactory(type_name='Document', id='sample', title='Sample Spotlight Item', description='This is a sample Spotlight Item', text='<p>You may delete this item</p>', subject=["spotlight-%s" % subsite.id])
 
-
-      
+    writeDebug('Creating events folder')      
 
     # Create Events folder
     if 'events' not in subsite.objectIds():
-        subsite.invokeFactory('Folder', id='events', title='Events')
+        subsite.invokeFactory(type_name='Folder', id='events', title='Events')
         obj = subsite['events']
         obj.setConstrainTypesMode(1) # restrict what this folder can contain
-        obj.setImmediatelyAddableTypes(['Event','Topic'])
-        obj.setLocallyAllowedTypes(['Event'])
+        obj.setImmediatelyAddableTypes(['Event'])
+        obj.setLocallyAllowedTypes(['Event','Topic'])
         obj.reindexObject()
+
+        # Create sample event
+        obj.invokeFactory(type_name='Event', id='sample', title='Sample Event', 
+                          description='This is a sample Event Item',  start_date='2020-01-01', 
+                          end_date='2020-01-01', start_time='13:00', stop_time='14:30', 
+                          event_url='http://agsci.psu.edu', location='Anywhere')
+
+        writeDebug('Creating upcoming collection')      
         
         # create a smartfolder for upcoming events inside the events folder, and set it as the default page
         if 'upcoming' not in obj.objectIds():
-            obj.invokeFactory('Topic', id='upcoming', title='Upcoming Events')
+            obj.invokeFactory(type_name='Topic', id='upcoming', title='Upcoming Events')
             obj.setDefaultPage('upcoming')
             
             smart_obj = obj['upcoming']
@@ -149,87 +228,102 @@ def onSubsiteCreation(subsite, event):
         
             sort_crit = smart_obj.addCriterion('start','ATSortCriterion')
 
-    #--------------------------------------------------------------------------------
-    # Stuff below is from rwellar
-    #--------------------------------------------------------------------------------
+    writeDebug('Setting portlets')
     
-   
-    # create a news folder with a smartfolder as a default page
-    if 'news' not in subsite.objectIds():
-        subsite.invokeFactory('Folder', id='news', title='News archive')
-        obj = subsite['news']
-        
-        if 'archive' not in obj.objectIds():
-            obj.invokeFactory('Topic', id='archive', title='News archive') # create a smartfolder
-            obj.setDefaultPage('archive') # set this as the default page for the containing folder
-        
-            smart_obj = obj['archive']
-            smart_obj.setLayout('folder_summary_view')
-            smart_obj.reindexObject()
-        
-            # state_crit = smart_obj.addCriterion('review_state','ATSimpleStringCriterion')
-            # state_crit.setValue('published')
-        
-            type_crit = smart_obj.addCriterion('Type','ATPortalTypeCriterion')
-            type_crit.setValue(['News Item'])
-        
-            path_crit = smart_obj.addCriterion('path','ATPathCriterion')
-            path_crit.setValue(subsite.UID()) # Only list news items in the current subsite
-            path_crit.setRecurse(True)
-        
-            sort_crit = smart_obj.addCriterion('effective','ATSortCriterion')
-            smart_obj.getSortCriterion().setReversed(True)
-
-        # let only news items be addable inside the news folder
-        obj.setConstrainTypesMode(1) 
-        obj.setImmediatelyAddableTypes(['News Item','Folder','Topic'])
-        obj.setLocallyAllowedTypes(['News Item','Folder','Topic'])
-        obj.reindexObject()
-    
-            
-    if 'default' not in subsite.objectIds():
-            subsite_title=str(subsite.title)
-            subsite.invokeFactory('Document',id='default',title=subsite_title, text='<p>Welcome to the '+subsite_title+'</p>')
-            subsite.setDefaultPage('default')
-    
+    # Set portlets
     # Register some portlets for this subsite's context.
     # Copied mostly from plone.portlets' README doctests.
-    right = getUtility(IPortletManager, name='plone.rightcolumn')
-    left = getUtility(IPortletManager, name='plone.leftcolumn')
-    rightColumnInThisContext = getMultiAdapter((subsite, right), IPortletAssignmentMapping)
-    leftColumnInThisContext = getMultiAdapter((subsite, left), IPortletAssignmentMapping)
+
+    subsite_LeftColumn = getPortletAssignmentMapping(subsite, 'plone.leftcolumn')
+    subsite_RightColumn = getPortletAssignmentMapping(subsite, 'plone.rightcolumn')
     
-    urltool  = getToolByName(subsite, 'portal_url')
-    # eventsCollectionPortlet = Assignment(header=u"Events",
-    #                                             limit=5,
-    #                                             target_collection = '/'.join(urltool.getRelativeContentPath(subsite.events.upcoming)),
-    #                                             random=False,
-    #                                             show_more=True,
-    #                                             show_dates=False)
-    #         
-    newsCollectionPortlet = Assignment(header=u"News",
-                                        limit=5,
-                                        target_collection = '/'.join(urltool.getRelativeContentPath(subsite.news.archive)),
-                                        random=False,
-                                        show_more=True,
-                                        show_dates=False)
-    
-    leftColumnInThisContext[u'navigation'] = navigation.Assignment(name=u"",
-                                            root='/'.join(urltool.getRelativeContentPath(subsite)),
+    # Block the parent portlets
+    writeDebug('Blocking parent portlets')
+
+    writeDebug('Blocking plone.leftcolumn parent portlets')    
+    try:
+        subsite_LeftColumnManager = getLocalPortletAssignmentManager(subsite, 'plone.leftcolumn')
+        subsite_LeftColumnManager.setBlacklistStatus(CONTEXT_CATEGORY, True)
+    except ComponentLookupError:
+        writeDebug('ERROR blocking plone.leftcolumn parent portlets')    
+
+    writeDebug('Blocking plone.rightcolumn parent portlets')    
+    try:
+        subsite_RightColumnManager = getLocalPortletAssignmentManager(subsite, 'plone.rightcolumn')
+        subsite_RightColumnManager.setBlacklistStatus(CONTEXT_CATEGORY, True)
+    except ComponentLookupError:
+        writeDebug('ERROR blocking plone.rightcolumn parent portlets')  
+        pdb.set_trace() 
+
+    # Set left navigation portlet
+    left_navigation = navigation.Assignment(name=u"",
+                                            root='/%s' % '/'.join(urltool.getRelativeContentPath(subsite)),
                                             currentFolderOnly = False,
                                             includeTop = True,
                                             topLevel = 0,
-                                            bottomLevel = 2)
+                                            bottomLevel = 3)
+
+    subsite_LeftColumn['navigation'] = left_navigation 
+
+    # Create homepage
+    if 'default' not in subsite.objectIds():
+        subsite_title=str(subsite.title)
+        subsite.invokeFactory(type_name='HomePage',id='default',title=subsite_title, text='')
+        subsite.setDefaultPage('default')
+        default=subsite['default']
+        
+        # Set portlets for homepage
+        homepage_centerColumn = getPortletAssignmentMapping(default, 'agcommon.centercolumn')
+        homepage_rightColumn = getPortletAssignmentMapping(default, 'agcommon.rightcolumn')
+
+        # Configure feedmixer in center column
+
+        # Grab path to news RSS feed, and make sure it's http://        
+        agsci_rss = 'http://agsci.psu.edu/news/live.psu.edu/agsci/RSS'
+        subsite_rss = subsite.absolute_url().replace('https:', 'http:') + '/news/latest/RSS'
+
+        if subsite_rss.count('localhost'):
+            feeds=[agsci_rss]
+        else:
+            feeds=[agsci_rss,subsite_rss]
+
+        subsite_news = feedmixer.portlet.Assignment(
+                    title="%s News" % subsite.title,
+                    feeds="\n".join(feeds),
+                    items_shown=3,
+                    show_header=True,
+                    show_date=True,
+                    show_summary=True,
+                    show_image=False,
+                    show_footer=False,
+                    cache_timeout=1800,
+                    assignment_context_path=None)
+                    
+        saveAssignment(homepage_centerColumn, subsite_news)
+
+        spotlightCollectionPortlet = collection.Assignment(header=u"Spotlight",
+                                        target_collection = '/'.join(urltool.getRelativeContentPath(subsite.news.spotlight.recent)),
+                                        random=False,
+                                        show_more=False,
+                                        show_dates=False)
+
+        saveAssignment(homepage_rightColumn, spotlightCollectionPortlet)
+                                        
+        eventsCollectionPortlet = collection.Assignment(header=u"Upcoming Events",
+                                        limit=3,
+                                        target_collection = '/'.join(urltool.getRelativeContentPath(subsite.events.upcoming)),
+                                        random=False,
+                                        show_more=True,
+                                        show_dates=True)
+
+        saveAssignment(homepage_rightColumn, eventsCollectionPortlet)
+                                        
+
+    writeDebug('Finished creating subsite')     
+    #pdb.set_trace() 
+    return True
+
     
-    def saveAssignment(mapping, assignment):
-        chooser = INameChooser(mapping)
-        mapping[chooser.chooseName(None, assignment)] = assignment
+
     
-    # saveAssignment(rightColumnInThisContext, eventsCollectionPortlet)
-    saveAssignment(rightColumnInThisContext, newsCollectionPortlet)
-    
-    # Block the parent portlets
-    subsiteLeftColumnManager = getMultiAdapter((subsite, left), ILocalPortletAssignmentManager)
-    subsiteLeftColumnManager.setBlacklistStatus(CONTEXT_CATEGORY, True)
-    subsiteRightColumnManager = getMultiAdapter((subsite, right), ILocalPortletAssignmentManager)
-    subsiteRightColumnManager.setBlacklistStatus(CONTEXT_CATEGORY, True) 
+
