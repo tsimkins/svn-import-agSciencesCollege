bbcd64377bd39d23c6a69c1d0e644a748b048dcc
trs22 <trs22@ea9a3244-bef9-0310-a2a5-f2a3d2dd1ec8>
2010-07-23 20:00:11 +0000

    Enhancements for the Twitter feed.

--- agSciencesCollege/agsci.apdfeeds/trunk/agsci/apdfeeds/browser/twitter_feed.py
+++ agSciencesCollege/agsci.apdfeeds/trunk/agsci/apdfeeds/browser/twitter_feed.py
@@ -7,6 +7,7 @@ from agsci.apdfeeds import apdfeedsMessageFactory as _
 from agsci.apdfeeds.browser.interfaces import IFeedDisplay
 from agsci.apdfeeds.twitter import getItems
 from Acquisition import aq_acquire, aq_inner
+import pdb
 
 class FeedDisplay(BrowserView):
     """
@@ -18,13 +19,30 @@ class FeedDisplay(BrowserView):
         
         self.context = context
         self.request = request
+        
+        # Get twitter_search
         try:
             search_term = aq_acquire(self.context, 'twitter_search')
         except AttributeError:
             search_term = '#pennstate'
         
+        # Get twitter_url
+        try:
+            twitter_url = aq_acquire(self.context, 'twitter_url')
+        except AttributeError:
+            twitter_url = ""
+            
+        # Get banned_ids
+        try:
+            banned_ids = aq_acquire(self.context, 'banned_ids')
+        except AttributeError:
+            banned_ids = []
+
+        banned_ids = [x.lower() for x in banned_ids]            
+        
         self.title = "Twitter Results For %s" % search_term
-        self.items = getItems(search_term)
+        
+        self.items = getItems(search_term=search_term, banned_ids=banned_ids, search_url=twitter_url)
 
     @property
     def portal_catalog(self):
--- agSciencesCollege/agsci.apdfeeds/trunk/agsci/apdfeeds/twitter.py
+++ agSciencesCollege/agsci.apdfeeds/trunk/agsci/apdfeeds/twitter.py
@@ -6,14 +6,18 @@ from datetime import datetime
 from pytz import utc, timezone
 from urllib import quote_plus
 
-def getItems(search_term, my_tz="US/Eastern"):
+def getItems(search_term="", banned_ids=[], search_url="", my_tz="US/Eastern"):
 
     results = []
     
     tz = timezone(my_tz)
     
-    feed_url = "http://search.twitter.com/search.atom?q=%s" % quote_plus(search_term)
-    
+    if search_url:
+        feed_url = search_url        
+    else:
+        feed_url = "http://search.twitter.com/search.atom?q=%s" % quote_plus(search_term)
+
+        
     feed = feedparser.parse(feed_url)
     
     current_time = datetime.now(tz)
@@ -35,11 +39,12 @@ def getItems(search_term, my_tz="US/Eastern"):
             item_time = datetime(*published_parsed).astimezone(tz)
             datestamp = datetime(*published_parsed).astimezone(tz).strftime('%m/%d/%Y %I:%M %p')
         
-        results.append({
-            'title' : title,
-            'author' : author,
-            'item_time' : "%s ago" % texttime.stringify(current_time - item_time),
-            'author_image' : author_image,
-        })
+        if not banned_ids.count(author.split()[0].lower()):
+            results.append({
+                'title' : title,
+                'author' : author,
+                'item_time' : "%s ago" % texttime.stringify(current_time - item_time),
+                'author_image' : author_image,
+            })
         
     return results
