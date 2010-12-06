2e0c409056ea1842377b1802ba8dbb2a3cbf1eb7
trs22 <trs22@ea9a3244-bef9-0310-a2a5-f2a3d2dd1ec8>
2010-12-06 21:29:26 +0000

    Updating to 1.3.0 branch

---
+++ agSciencesCollege/collective.contentleadimage/branches/1.3.0-agsci/collective/contentleadimage/browser/folder_leadimage_view.py
@@ -0,0 +1,50 @@
+from Acquisition import aq_inner, aq_acquire
+from zope.component import getUtility, getMultiAdapter
+from Products.Five import BrowserView
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from Products.CMFPlone.interfaces import IPloneSiteRoot
+from collective.contentleadimage.config import IMAGE_FIELD_NAME
+from collective.contentleadimage.config import IMAGE_CAPTION_FIELD_NAME
+from collective.contentleadimage.leadimageprefs import ILeadImagePrefsForm
+
+class FolderLeadImageView(BrowserView):
+    
+    template = ViewPageTemplateFile('folder_leadimage_view.pt')
+    template.id = "folder_leadimage_view"
+
+    @property
+    def prefs(self):
+        portal = getUtility(IPloneSiteRoot)
+        return ILeadImagePrefsForm(portal)
+
+    def tag(self, obj, css_class='tileImage'):
+        context = aq_inner(obj)
+        field = context.getField(IMAGE_FIELD_NAME)
+        titlef = context.getField(IMAGE_CAPTION_FIELD_NAME)
+        if titlef is not None:
+            title = titlef.get(context)
+        else:
+            title = ''
+        if field is not None:
+            if field.get_size(context) != 0:
+                scale = self.prefs.desc_scale_name
+                return field.tag(context, scale=scale, css_class=css_class, title=title)
+        return ''
+
+    def __init__(self, context, request):
+        BrowserView.__init__(self, context, request)
+        self.update()
+        
+    def __call__(self):
+        return self.template()
+        
+    def update(self):
+        context_state = getMultiAdapter((self.context, self.request),
+                                        name=u'plone_context_state')
+                                        
+        try:
+            show_date = aq_acquire(self.context, 'show_date')
+        except AttributeError:
+            show_date = False
+        
+        self.show_date = show_date
--- agSciencesCollege/collective.contentleadimage/branches/1.3.0/collective/contentleadimage/browser/viewlets.py
+++ agSciencesCollege/collective.contentleadimage/branches/1.3.0/collective/contentleadimage/browser/viewlets.py
@@ -7,10 +7,6 @@ from collective.contentleadimage.config import IMAGE_FIELD_NAME
 from collective.contentleadimage.config import IMAGE_CAPTION_FIELD_NAME
 from collective.contentleadimage.leadimageprefs import ILeadImagePrefsForm
 
-# We're going to display the lead image first, and then the news item image.
-# This gets the news item image out of the body and into the content lead image viewlet
-# for standardization purposes.
-
 class LeadImageViewlet(ViewletBase):
     """ A simple viewlet which renders leadimage """
 
@@ -19,21 +15,10 @@ class LeadImageViewlet(ViewletBase):
         portal = getUtility(IPloneSiteRoot)
         return ILeadImagePrefsForm(portal)
 
-    def bodyTag(self, css_class=''):
+    def bodyTag(self, css_class='newsImage'):
         """ returns img tag """
         context = aq_inner(self.context)
-        
-        # Use News Item image as Content Lead Image, if it exists
-        field = None
-        
-        leadimagefield = context.getField(IMAGE_FIELD_NAME)
-        newsitemfield =  context.getField('image')
-
-        if leadimagefield:
-            field = leadimagefield
-        if newsitemfield:
-            field = newsitemfield
-        
+        field = context.getField(IMAGE_FIELD_NAME)
         if field is not None and \
           field.getFilename(context) is not None and \
             field.get_size(context) != 0:
@@ -44,18 +29,7 @@ class LeadImageViewlet(ViewletBase):
     def descTag(self, css_class='tileImage'):
         """ returns img tag """
         context = aq_inner(self.context)
-
-        # Use News Item image as Content Lead Image image, if it exists
-        field = None
-        
-        leadimagefield = context.getField(IMAGE_FIELD_NAME)
-        newsitemfield =  context.getField('image')
-
-        if leadimagefield:
-            field = leadimagefield
-        if newsitemfield:
-            field = newsitemfield
-            
+        field = context.getField(IMAGE_FIELD_NAME)
         if field is not None and \
           field.getFilename(context) is not None and \
             field.get_size(context) != 0:
@@ -65,27 +39,12 @@ class LeadImageViewlet(ViewletBase):
 
     def caption(self):
         context = aq_inner(self.context)
-        
-        # Use News Item caption as Content Lead Image caption, if it exists
-        if context.getField(IMAGE_FIELD_NAME):
-            return context.widget(IMAGE_CAPTION_FIELD_NAME, mode='view')
-        elif context.getField('imageCaption'):
-            return context.widget('imageCaption', mode='view')
-        else:
-            return ''
-        
+        return context.widget(IMAGE_CAPTION_FIELD_NAME, mode='view')
+
     def render(self):
         context = aq_inner(self.context)
         portal_type = getattr(context, 'portal_type', None)
-        
-        # Special case for News Item
-        if portal_type in self.prefs.allowed_types or portal_type == 'News Item':
+        if portal_type in self.prefs.allowed_types:
             return super(LeadImageViewlet, self).render()
         else:
             return ''
-
-    # Used in page template to determine if we're working with a news item
-    def isNewsItem(self):
-        context = aq_inner(self.context)
-        portal_type = getattr(context, 'portal_type', None)
-        return portal_type == 'News Item'
