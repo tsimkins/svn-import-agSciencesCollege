86aaa11cb126f42b1b592cf11da4387c3095d893
trs22 <trs22@ea9a3244-bef9-0310-a2a5-f2a3d2dd1ec8>
2011-03-01 21:52:27 +0000

    Set caching parameters in magic .pngs

--- agSciencesCollege/agCommon/branches/3.0.1/skins/agcommon_templates/gradientBackground.py
+++ agSciencesCollege/agCommon/branches/3.0.1/skins/agcommon_templates/gradientBackground.py
@@ -1,9 +1,21 @@
 from Products.agCommon import gradientBackground
+from DateTime import DateTime
 
 request = container.REQUEST
 RESPONSE =  request.RESPONSE
 
 RESPONSE.setHeader('Content-Type', 'image/png')
+RESPONSE.setHeader('Cache-Control', 'max-age=86400, s-maxage=86400, public, must-revalidate, proxy-revalidate')
+
+# From http://www.peterbe.com/plog/cache-control_or_expires
+hours = 24
+now = DateTime() 
+then = now+int(hours/24.0)
+RESPONSE.setHeader('Expires',then.rfc822()) 
+
+modified = now-int(hours/24.0)
+RESPONSE.setHeader('Last-Modified',modified.rfc822()) 
+
 print gradientBackground(request)
 
 return printed
