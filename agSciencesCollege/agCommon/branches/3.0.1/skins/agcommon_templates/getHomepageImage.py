24bab2cb973907b511304fadf3d1a3a957ce22a3
trs22 <trs22@ea9a3244-bef9-0310-a2a5-f2a3d2dd1ec8>
2011-03-08 21:59:26 +0000

    Adding caching parameters to 'magic' .js files.

--- agSciencesCollege/agCommon/branches/3.0.1/skins/agcommon_templates/getHomepageImage.py
+++ agSciencesCollege/agCommon/branches/3.0.1/skins/agcommon_templates/getHomepageImage.py
@@ -1,4 +1,10 @@
 from Products.agCommon import getHomepageImage
 
+request = container.REQUEST
+RESPONSE =  request.RESPONSE
+
+RESPONSE.setHeader('Content-Type', 'application/x-javascript')
+RESPONSE.setHeader('Cache-Control', 'max-age=3600, s-maxage=3600, public, must-revalidate, proxy-revalidate')
+
 print getHomepageImage(context)
 return printed
--- agSciencesCollege/agCommon/branches/3.0.1/skins/agcommon_templates/getPortletHomepageImage.py
+++ agSciencesCollege/agCommon/branches/3.0.1/skins/agcommon_templates/getPortletHomepageImage.py
@@ -1,4 +1,10 @@
 from Products.agCommon import getPortletHomepageImage
 
+request = container.REQUEST
+RESPONSE =  request.RESPONSE
+
+RESPONSE.setHeader('Content-Type', 'application/x-javascript')
+RESPONSE.setHeader('Cache-Control', 'max-age=3600, s-maxage=3600, public, must-revalidate, proxy-revalidate')
+
 print getPortletHomepageImage(context)
 return printed
