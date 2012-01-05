a18ea00f60ee420e57b57906167566283bd8a5c2
trs22 <trs22@ea9a3244-bef9-0310-a2a5-f2a3d2dd1ec8>
2012-01-05 21:02:12 +0000

    Tweaked caching for newsitem_redirect_view.

--- agSciencesCollege/agCommon/branches/3.0.1/skins/agcommon_templates/newsitem_redirect_view.py
+++ agSciencesCollege/agCommon/branches/3.0.1/skins/agcommon_templates/newsitem_redirect_view.py
@@ -8,6 +8,7 @@ Redirect to the News Item's website target URL, if and only if:
 """
 
 from Products.CMFCore.utils import getToolByName
+from zope.component import getMultiAdapter
 
 mtool = getToolByName(context, 'portal_membership')
 
@@ -25,8 +26,13 @@ else:
     # newsitem_view.pt is a template in the plone_content skin layer
     request = container.REQUEST
     RESPONSE =  request.RESPONSE
-    if can_edit:
+
+    portal_state = getMultiAdapter((context, request), name=u'plone_portal_state')
+    anonymous = portal_state.anonymous()    
+    
+    if not anonymous:
         RESPONSE.setHeader('Cache-Control', 'max-age=0, must-revalidate, private')
     else:
         RESPONSE.setHeader('Cache-Control', 'max-age=0, s-maxage=86400, must-revalidate, public, proxy-revalidate')
+
     return context.newsitem_view()
