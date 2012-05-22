93b6e74925cd857daeb5874406b385c1ad45c070
trs22 <trs22@ea9a3244-bef9-0310-a2a5-f2a3d2dd1ec8>
2012-05-22 12:01:37 +0000

    * Fixed JS bug in IE  * Added tests

--- agSciencesCollege/agsci.blognewsletter/trunk/agsci/blognewsletter/tests/test_views.py
+++ agSciencesCollege/agsci.blognewsletter/trunk/agsci/blognewsletter/tests/test_views.py
@@ -4,7 +4,9 @@ from plone.app.testing.layers import IntegrationTesting
 import unittest2 as unittest
 from plone.testing import z2
 import transaction
-
+from Products.Five.browser import BrowserView
+from zope.component import queryMultiAdapter
+from zope.viewlet.interfaces import IViewletManager
 from plone.app.testing import TEST_USER_ID, TEST_USER_NAME, login, setRoles
 
 # Plone imports
@@ -71,7 +73,7 @@ class BlogNewsletterLayer(PloneSandboxLayer):
         #Apparently, we need to tell Plone what it's default workflow is before this will work.
         portal_workflow.setDefaultChain('simple_publication_workflow')
 
-        childObjects = [x[1] for x in portal.ZopeFind(blog)]
+        childObjects = [x[1] for x in portal.ZopeFind(blog, search_sub=1)]
         childObjects.append(blog)
 
         for obj in childObjects:
@@ -79,14 +81,14 @@ class BlogNewsletterLayer(PloneSandboxLayer):
                 if not portal_workflow.getInfoFor(obj, 'review_state').lower().count('publish'):
                     portal_workflow.doActionFor(obj, 'publish')
             except WorkflowException:
-                import pdb; pdb.set_trace()
+                continue # Must not support workflow
 
         transaction.commit()
-        
+
         # Assign tag to sample news item
         for obj in childObjects:
             if obj.id == 'sample':
-                obj.public_tags = ('sample-tag', )
+                obj.public_tags = ('Sample Tag', )
                 obj.reindexObject()
 
         transaction.commit()
@@ -103,14 +105,47 @@ class ViewTests(unittest.TestCase):
     def setUp(self):
         self.portal = self.layer['portal']
         self.request = self.layer['request']
+        # Creating empty value for ACTUAL_URL to prevent KeyError related to 
+        # view/prepareObjectTabs
+        self.request['ACTUAL_URL'] = ''
 
-    def test_newsletter_view(self):
+    def test_newsletter_email(self):
         resource = self.portal.blog.newsletter.restrictedTraverse('@@newsletter_email')
         resource()
 
-    def test_tags_view(self):
-        resource = self.portal.blog.latest.restrictedTraverse('@@tags')
+    def test_newsletter_view(self):
+        resource = self.portal.blog.newsletter.restrictedTraverse('@@newsletter_view')
         resource()
+
+    def test_newsletter_print(self):
+        resource = self.portal.blog.newsletter.restrictedTraverse('@@newsletter_print')
+        resource()
+
+    def test_tags_view(self):
+        resource = self.portal.blog.latest.unrestrictedTraverse('@@tags')
+        resource()
+
+class ViewletTests(unittest.TestCase):
+
+    layer = BLOGNEWSLETTER_INTEGRATION_TESTING
+
+    def setUp(self):
+        self.portal = self.layer['portal']
+        self.request = self.layer['request']
+
+    def test_tags_viewlet(self):
+        # Grab the sample news item
+        context = self.portal.blog.latest.queryCatalog()[0].getObject()
+        request = self.request
+        view = BrowserView(context, request)
+        manager_name = "plone.belowcontenttitle"
+        manager = queryMultiAdapter((context, request, view), IViewletManager, manager_name, default=None)
+        self.failUnless(manager)
+        manager.update()
+        my_viewlet = [v for v in manager.viewlets if v.__name__ == 'agsci.blognewsletter.tags']
+        self.failUnlessEqual(len(my_viewlet), 1)
+        self.failUnless('Sample Tag' in my_viewlet[0].render())
+
 
 def test_suite():
     return unittest.defaultTestLoader.loadTestsFromName(__name__)
