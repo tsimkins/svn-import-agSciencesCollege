f0084de83c8243b2cbba2d359229cee58e8e5da0
trs22 <trs22@ea9a3244-bef9-0310-a2a5-f2a3d2dd1ec8>
2012-06-01 19:50:44 +0000

    Added fixHeadingTags

--- agSciencesCollege/agsci.w3c/agsci/w3c/content.py
+++ agSciencesCollege/agsci.w3c/agsci/w3c/content.py
@@ -7,16 +7,24 @@ import re
 
 
 def setText(object, text):
-    if object.portal_type in ['Folder']:
+    if object.portal_type in ['Folder', 'PhotoFolder'] or object.meta_type in ['Blog', 'Subsite', 'Section', 'ATFolder', 'PhotoFolder', 'FormFolder']:
         object.folder_text.update(text, object)
+    elif object.portal_type in ['FSDPerson']:
+        object.setBiography(text)
+    elif object.portal_type in ['File', 'Image', 'Link', 'FSDFacultyStaffDirectoryTool'] or object.portal_type.startswith('Form'):
+        pass
     else:
         object.setText(text)
     object.reindexObject()
 
 
 def getText(object):
-    if object.portal_type in ['Folder']:
+    if object.portal_type in ['Folder', 'PhotoFolder'] or object.meta_type in ['Blog', 'Subsite', 'Section', 'ATFolder', 'PhotoFolder', 'FormFolder']:
         text = object.folder_text()
+    elif object.portal_type in ['File', 'Image', 'Link', 'FSDFacultyStaffDirectoryTool'] or object.portal_type.startswith('Form'):
+        text = ''
+    elif object.portal_type in ['FSDPerson']:
+        text = object.getBiography()
     else:
         text = object.getText()
     return text
@@ -24,9 +32,59 @@ def getText(object):
 
 def getItemsWithText(context):
     portal_catalog = getToolByName(context, "portal_catalog")
-    
-    return portal_catalog.searchResults({'portal_type' : ['News Item', 'Event', 'Document', 'Folder', 'Topic']})
 
+    return portal_catalog.searchResults({'portal_type' : ['News Item', 'Event', 'Document', 'Folder', 'Topic', 'HomePage', 'PhotoFolder', 'FSDPerson']})
+
+def fixHeadingTags(context, dry_run=True):
+    report = Report(categories=['headingsOK', 'headingsBold', 'headingsItalic', 'headingsBr'])
+
+    def getNewTag(tag, heading):
+        tag_regex = re.compile("</*%s>" % tag, re.I|re.M)
+        return BeautifulSoup(tag_regex.sub('', str(heading))).contents[0]
+
+    for r in getItemsWithText(context):
+        o = r.getObject()
+        text = getText(o)
+        soup = BeautifulSoup(text)
+        old_soup = soup.prettify()
+        found_italic = False
+        found_bold = False
+        found_br = False
+        
+        for htag in ['h2', 'h3']:
+            for h in soup.findAll(htag):
+                for t in ['strong', 'b']:
+                    if h(t):
+                        found_bold = True
+                        h.replaceWith(getNewTag(t, h))
+
+                for t in ['br']:
+                    for tag in h(t):
+                        found_br = True
+                        tag.extract()
+
+                for t in ['em', 'i']:
+                    if h(t):
+                        found_italic = True
+
+        if found_bold:
+            report.add('headingsBold', o)
+
+        if found_italic:
+            report.add('headingsItalic', o)
+            
+        if found_br:
+            report.add('headingsBr', o)
+            
+        new_soup = soup.prettify()
+        
+        if not dry_run and (found_bold or found_br):
+            setText(o, new_soup)
+            
+        #open("/tmp/fixh/bad/%s" % o.UID(), "w").write(old_soup)
+        #open("/tmp/fixh/good/%s" % o.UID(), "w").write(new_soup)
+                    
+    return report
 
 def fixPotentialHeadings(context, dry_run=True):
     report = Report(categories=['hasAutoFixableHeadings', 'hasManualFixableHeadings', 'noHeadings'])
@@ -39,8 +97,11 @@ def fixPotentialHeadings(context, dry_run=True):
         found_auto=False
         found_manual=False
         replaced_text = []
-        
+
         for p in soup.findAll('p'):
+            for c in ['\n', '\r', '\r\n']:
+                while c in p.contents:
+                    p.contents.remove(c)
             if len(p.contents) == 1:
                 if isinstance(p.contents[0], Tag):
                     if p.contents[0].name in ['strong', 'b'] and len(p.contents[0]) == 1:
@@ -57,32 +118,35 @@ def fixPotentialHeadings(context, dry_run=True):
             report.add('hasManualFixableHeadings', o)
         else:
             report.add('noHeadings', o)
-                                
+
         if found_auto and not dry_run:
             new_soup = soup.prettify()
             setText(o, new_soup)
 
     return report
-    
-    
+
+
 
 def fixH2H3(context, dry_run=True):
 
     h3 = re.compile(r"<(/*)h3(.*?)>")
-    
+
     report = Report(categories=['rightHeadings', 'wrongHeadings', 'noHeadings'])
-    
+
     for r in getItemsWithText(context):
         o = r.getObject()
         text = getText(o)
-        soup = BeautifulSoup(text)
+        try:
+            soup = BeautifulSoup(text)
+        except TypeError:
+            import pdb; pdb.set_trace()
         if soup('h2'):
             report.add('rightHeadings', o)
         elif soup('h3'):
             report.add('wrongHeadings', o)
         else:
             report.add('noHeadings', o)
-    
+
     if not dry_run:
         for o in report.get('wrongHeadings'):
             text = getText(o)
@@ -91,7 +155,7 @@ def fixH2H3(context, dry_run=True):
             setText(o, new_soup)
 
     return report
-    
+
 def fixLinkText(context, dry_run=True):
 
     report = Report(categories=['noLinks', 'okLinks', 'brokenLinks'])
@@ -103,7 +167,7 @@ def fixLinkText(context, dry_run=True):
         found_link = False
         found_empty_link = False
         found_no_links = False
-                
+
         if soup('a'):
             for a in soup.findAll('a'):
                 for i in a.contents:
@@ -111,7 +175,7 @@ def fixLinkText(context, dry_run=True):
                         link_text = i
                     else:
                         link_text = link_text
-                        
+
                     if link_text and 'http://' in link_text or 'https://' in link_text:
                         found_link = True
                     elif not link_text:
@@ -119,13 +183,13 @@ def fixLinkText(context, dry_run=True):
                         found_empty_link = True
         else:
             found_no_links = True
-            
+
         if found_no_links:
             report.add('noLinks', o)
         elif found_link:
             report.add('brokenLinks', o)
         else:
-            report.add('okLinks', o)                
+            report.add('okLinks', o)
 
         # Note: emptyLinks are also included in broken links.
         if found_empty_link:
