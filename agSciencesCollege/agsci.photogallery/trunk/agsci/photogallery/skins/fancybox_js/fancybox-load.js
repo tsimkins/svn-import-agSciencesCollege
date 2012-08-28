8b11e5459033bd949c7af96f6373572682ede45c
trs22 <trs22@ea9a3244-bef9-0310-a2a5-f2a3d2dd1ec8>
2012-08-28 13:20:58 +0000

    Fixing JS for lead image.

--- agSciencesCollege/agsci.photogallery/trunk/agsci/photogallery/skins/fancybox_js/fancybox-load.js
+++ agSciencesCollege/agsci.photogallery/trunk/agsci/photogallery/skins/fancybox_js/fancybox-load.js
@@ -2,7 +2,13 @@ jq(document).ready(function() {
     jq(".contentLeadImageContainer a#parent-fieldname-leadImage").each(
         function () {
             var href = jq(this).attr('href');
-            if (href.indexOf('image_view_fullscreen') >= 0)
+            if (href.indexOf('leadImage/image_view_fullscreen') >= 0)
+            {
+                href = href.replace('leadImage/image_view_fullscreen', 'leadImage_galleryzoom');
+                jq(this).attr('href', href);
+                jq(this).addClass('fancybox');
+            }
+            else if (href.indexOf('image_view_fullscreen') >= 0)
             {
                 href = href.replace('image_view_fullscreen', 'image_galleryzoom');
                 jq(this).attr('href', href);
