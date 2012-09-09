76148397ee52c55a960d1a64c49abd5e68b7db0f
trs22 <trs22@ea9a3244-bef9-0310-a2a5-f2a3d2dd1ec8>
2012-09-09 23:37:31 +0000

    Additional methods, tweaks to existing ones.

--- agSciencesCollege/agsci.w3c/agsci/w3c/content.py
+++ agSciencesCollege/agsci.w3c/agsci/w3c/content.py
@@ -1,5 +1,6 @@
 from Products.CMFCore.utils import getToolByName
 from BeautifulSoup import BeautifulSoup, Tag, NavigableString
+from zope.app.component.hooks import getSite
 from agsci.w3c import Report
 import re
 
@@ -8,7 +9,10 @@ import re
 
 def setText(object, text):
     if object.portal_type in ['Folder', 'PhotoFolder'] or object.meta_type in ['Blog', 'Subsite', 'Section', 'ATFolder', 'PhotoFolder', 'FormFolder']:
-        object.folder_text.update(text, object)
+        try:
+            object.folder_text.update(text, object)
+        except AttributeError:
+            pass
     elif object.portal_type in ['FSDPerson']:
         object.setBiography(text)
     elif object.portal_type in ['File', 'Image', 'Link', 'FSDFacultyStaffDirectoryTool'] or object.portal_type.startswith('Form'):
@@ -20,7 +24,10 @@ def setText(object, text):
 
 def getText(object):
     if object.portal_type in ['Folder', 'PhotoFolder'] or object.meta_type in ['Blog', 'Subsite', 'Section', 'ATFolder', 'PhotoFolder', 'FormFolder']:
-        text = object.folder_text()
+        try:
+            text = object.folder_text()
+        except AttributeError:
+            text = ""
     elif object.portal_type in ['File', 'Image', 'Link', 'FSDFacultyStaffDirectoryTool'] or object.portal_type.startswith('Form'):
         text = ''
     elif object.portal_type in ['FSDPerson']:
@@ -160,13 +167,83 @@ def fixH2H3(context, dry_run=True):
 
         for o in report.get('wrongHeadings'):
             text = getText(o)
-            soup = BeautifulSoup(text).prettify()
-            new_soup = h3.sub(r"<\g<1>h2\g<2>>", soup)
+            new_soup = BeautifulSoup(text).prettify()
+            old_soup = new_soup
+            new_soup = h3.sub(r"<\g<1>h2\g<2>>", new_soup)
             report.addDiff(o, old_soup, new_soup)
             if not dry_run:
                 setText(o, new_soup)
 
     return report
+
+
+def fixOtherHeadingLevels(context, dry_run=True):
+
+    report = Report(categories=['goodHeadings', 'badHeadings', 'goodAndBadHeadings', 'noHeadings', 'tooManyHeadings'])
+
+    good_headings = set(['h2', 'h3'])
+    bad_headings = set(['h1', 'h4', 'h5', 'h6'])
+    all_headings = list(good_headings.union(bad_headings))
+
+
+    for r in getItemsWithText(context):
+        o = r.getObject()
+        text = getText(o)
+        
+        headings = {}
+
+        try:
+            soup = BeautifulSoup(text)
+        except TypeError:
+            import pdb; pdb.set_trace()
+
+        has_good = has_bad = False
+
+        for h in all_headings:
+            heading_count = len(soup(h))
+            if heading_count:
+                headings[h] = heading_count
+                if h in good_headings:
+                    has_good = True
+                elif h in bad_headings:
+                    has_bad = True
+        
+        if has_good and has_bad:
+            report.add('goodAndBadHeadings', o)
+        elif has_good:
+            report.add('goodHeadings', o)
+        elif has_bad:
+            report.add('badHeadings', o)
+        else:                
+            report.add('noHeadings', o)
+
+        if len(headings.keys()) > 3:
+            report.add('tooManyHeadings', o)
+        elif has_bad:
+            if len(headings.keys()) == 3:
+                headings_to_fix = sorted(headings.keys())
+                heading_replacements = zip(headings_to_fix, ['h2', 'h3', 'h4'])
+            elif len(headings.keys()) == 2:
+                headings_to_fix = sorted(headings.keys())
+                heading_replacements = zip(headings_to_fix, ['h2', 'h3'])
+            else:
+                heading_replacements = [(headings.keys()[0], 'h2'),]
+
+            old_soup = soup.prettify()
+            new_soup = old_soup
+            for (from_h, to_h) in heading_replacements:
+                if from_h == to_h:
+                    continue
+                replaceHeading = re.compile(r"<(/*)%s(.*?)>" % from_h)
+                new_soup = replaceHeading.sub(r'<\1%s>' % to_h, new_soup)                
+            if old_soup != new_soup:
+                report.addDiff(o, old_soup, new_soup)
+                if not dry_run:
+                    setText(o, new_soup)
+            
+            
+    return report
+
 
 def fixLinkText(context, dry_run=True):
 
@@ -220,5 +297,93 @@ def fixLinkText(context, dry_run=True):
         if found_empty_link:
             report.add('emptyLinks', o)
 
+
+    return report
+
+
+def fixResolveUID(context, dry_run=True):
+
+    portal_catalog = getToolByName(context, 'portal_catalog')
+    report = Report(categories=['hasResolveUID', 'brokenResolveUID', 'noResolveUID'])
+
+    uids = {}
+    site_url = getSite().absolute_url()
+    
+    def getURLByUIDLink(uid):
+        # Preparsing
+        anchor = ''
+        image_size = ''
+        
+        if '#' in uid:
+            (uid,anchor)  = uid.split('#')[0:2]
+            if anchor:
+                anchor = '#%s' % anchor
+
+        if '/image_' in uid or '/leadImage_' in uid:
+            (uid,image_size)  = uid.split('/')[0:2]
+            if image_size:
+                image_size = '/%s' % image_size
+                
+        if uids.get(uid):
+            return uids[uid]
+        else:
+            results = portal_catalog.searchResults({'UID' : uid})
+
+            if results:
+                url = results[0].getURL().replace(site_url, '', 1)
+                uids[uid] = "".join([url, image_size, anchor])
+            else:
+                uids[uid] = None
+
+            return uids[uid]
+
+    for r in getItemsWithText(context):
+        o = r.getObject()
+        text = getText(o)
+        has_uid = False
+        broken_uid = False
+
+        soup = BeautifulSoup(text)
+
+        old_soup = soup.prettify()
+
+        for a in soup('a'):
+            href = a.get('href', '')
+            if href.startswith('resolveuid/'):
+                uid = href.replace('resolveuid/', '')
+                url = getURLByUIDLink(uid)
+                if url:
+                    a['href'] = url
+                    has_uid = True
+                else:
+                    url = "NOT_FOUND"
+                    broken_uid = True
+                print "A   %s -> %s" % (uid, url)
+
+        for i in soup('img'):
+            src = i.get('src', '')
+            if src.startswith('resolveuid/'):
+                uid = src.replace('resolveuid/', '')
+                url = getURLByUIDLink(uid)
+                if url:
+                    i['src'] = url
+                    has_uid = True
+                else:
+                    url = "NOT_FOUND"
+                    broken_uid = True
+                print "IMG %s -> %s" % (uid, url)
+
+        if broken_uid:
+            report.add('brokenResolveUID', o)
+
+        if has_uid:
+            report.add('hasResolveUID', o)
+            new_soup = soup.prettify()
+            report.addDiff(o, old_soup, new_soup)
+            if not dry_run:
+                setText(o, new_soup)
+                o.reindexObject()
+        else:
+            report.add('noResolveUID', o)
 
     return report
