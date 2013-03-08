dc77c18cb9b8e3b732a987212e48b9a466e9683e
trs22 <trs22@ea9a3244-bef9-0310-a2a5-f2a3d2dd1ec8>
2013-03-08 00:15:23 +0000

    * Additional course functionality  * Adding Extension fields to collections

--- agSciencesCollege/agsci.ExtensionExtender/trunk/agsci/ExtensionExtender/tools/course.py
+++ agSciencesCollege/agsci.ExtensionExtender/trunk/agsci/ExtensionExtender/tools/course.py
@@ -5,43 +5,40 @@ from OFS.SimpleItem import SimpleItem
 from AccessControl import ClassSecurityInfo
 from Products.CMFCore.utils import getToolByName
 import xlrd
-from plone.registry import field
-from plone.registry.interfaces import IRegistry
-from plone.registry.record import Record
-from plone.registry.registry import Registry
+import sqlite3
+import Zope2
 
 class ExtensionCourseTool(UniqueObject, SimpleItem):
 
     id = 'extension_course_tool'
     meta_type = 'Extension Course Tool'
-
+    
     security = ClassSecurityInfo()
 
     configFile = 'extension_courses.xls'
 
-    @property
-    def registry(self):
-        return getUtility(IRegistry)
-
-    def getRegistryKey(self, key):
-        return "%s_%s" % (self.id, key)
-
-    def __init__(self):
-        pass
-
+    def conn(self):
+        sqlite_dir = Zope2.os.getenv('SQLITE_DBDIR')
+        return sqlite3.connect('%s/extension_courses.db' % sqlite_dir)
+    
     security.declarePrivate('loadCourses')
     def loadCourses(self):
+        conn = self.conn()
+        c = conn.cursor()
+
+        c.execute("""drop table if exists courses""")
+        c.execute("""create table courses (category text, topic text, subtopic text, course text, description text, body_text text)""")
+
         portal_skins = getToolByName(self, 'portal_skins')
         paths = portal_skins.getSkinPath(portal_skins.getDefaultSkin()).split(',')
+
         o = None
+
         for p in paths:
             if p in portal_skins.objectIds() and self.configFile in portal_skins[p].objectIds():
                 o = portal_skins[p][self.configFile]
-        if o:
-            course_data = {}
-            topic_data = {}
-            subtopic_data = {}
 
+        if o:
             xls = xlrd.open_workbook(file_contents=o._readFile(False))
             sheet = xls.sheet_by_index(0)
             header = [x.value.strip() for x in sheet.row(0)]
@@ -50,79 +47,185 @@ class ExtensionCourseTool(UniqueObject, SimpleItem):
             category_col = header.index('Category')
             topic_col = header.index('Topic')
             subtopic_col = header.index('Subtopic')
-            subtopic_col = header.index('Description')
-            subtopic_col = header.index('Body Text')
+            description_col = header.index('Description')
+            body_text_col = header.index('Body Text')
                         
             for r in range(1,sheet.nrows):
                 row = sheet.row(r)
 
                 course_name = row[course_col].value.strip()
                 category_name = row[category_col].value.strip()
-                topic_name = "%s:%s" % (category_name, row[topic_col].value.strip())
+                topic_name = row[topic_col].value.strip()
                 subtopic_name = row[subtopic_col].value.strip()
+                description_name = row[description_col].value.strip()
+                body_text_name = row[body_text_col].value.strip()
 
-                if subtopic_name:
-                    subtopic_name = "%s:%s" % (topic_name, subtopic_name)
+                c.execute("insert into courses (category, topic, subtopic, course, description, body_text) values (?, ?, ?, ?, ?, ?)", 
+                          (category_name, topic_name, subtopic_name, course_name, description_name, body_text_name))
 
-                if not course_data.get(course_name):
-                    course_data[course_name] = []
+        conn.commit()
+        conn.close()
 
-                if not topic_data.get(topic_name):
-                    topic_data[topic_name] = []
-
-                if not subtopic_data.get(subtopic_name):
-                    subtopic_data[subtopic_name] = []
-
-                i_course_data = {}
-
-                for c in range(0,sheet.ncols):
-                    h = header[c]
-                    i_course_data[h] = row[c].value.strip()
-
-                course_data[course_name].append(i_course_data)
-                topic_data[topic_name].append(i_course_data)
-                subtopic_data[subtopic_name].append(i_course_data)
-
-            self.registry.records[self.getRegistryKey('course')] = Record(field.Dict(title=u"Course Data"), course_data)
-            self.registry.records[self.getRegistryKey('topic')] = Record(field.Dict(title=u"Topic Data"), topic_data)
-            self.registry.records[self.getRegistryKey('subtopic')] = Record(field.Dict(title=u"Subtopic Data"), subtopic_data)
+        
 
     security.declarePublic('getCourses')
     def getCourses(self):
-        data = self.registry.records[self.getRegistryKey('course')]
-        return sorted(data.value.keys())
+        conn = self.conn()
+
+        c = conn.cursor()
+
+        results = c.execute("""select distinct course from courses order by course""").fetchall()
+
+        conn.close()
+
+        return [x[0] for x in results]
+
 
     security.declarePublic('getCourseInfo')
     def getCourseInfo(self, course):
-        data = self.registry.records[self.getRegistryKey('course')]
-        return data.value.get(course)
+        
+        conn = self.conn()
+
+        c = conn.cursor()
+
+        results = c.execute("""select course, description, body_text from courses where course = ? limit 1""", (course,)).fetchone()
+
+        conn.close()
+
+        return results
+
+
+    security.declarePublic('getCoursesByCategory')
+    def getCoursesByCategory(self, category):
+        
+        conn = self.conn()
+
+        c = conn.cursor()
+
+        results = c.execute("""select course from courses where category = ? order by course""", (category,)).fetchall()
+
+        conn.close()
+
+        return results
 
     security.declarePublic('getCoursesByTopic')
-    def getCoursesByTopic(self, topic):
-        data = self.registry.records[self.getRegistryKey('topic')]
-        return [x.get('Course') for x in data.value.get(topic)]
+    def getCoursesByTopic(self, category, topic):
+        
+        conn = self.conn()
+
+        c = conn.cursor()
+
+        results = c.execute("""select course from courses where category = ? and topic = ? order by course""", (category,topic)).fetchall()
+
+        conn.close()
+
+        return results
+
 
     security.declarePublic('getCoursesBySubtopic')
-    def getCoursesBySubtopic(self, subtopic):
-        data = self.registry.records[self.getRegistryKey('subtopic')]
-        return [x.get('Course') for x in data.value.get(subtopic)]
+    def getCoursesBySubtopic(self, category, topic, subtopic):
+        
+        conn = self.conn()
+
+        c = conn.cursor()
+
+        results = c.execute("""select course from courses where category = ? and topic = ? and subtopic = ? order by course""", (category,topic,subtopic)).fetchall()
+
+        conn.close()
+
+        return results
+
+
+    security.declarePublic('getCourseCategory')
+    def getCourseCategory(self, course):
+        
+        conn = self.conn()
+
+        c = conn.cursor()
+
+        results = c.execute("""select distinct category from courses where course = ? order by category""", (course,)).fetchall()
+
+        conn.close()
+
+        return results
+
 
     security.declarePublic('getCourseTopics')
     def getCourseTopics(self, course):
-        data = self.getCourseInfo(course)
-        if data:
-            return [":".join([x.get('Category'), x.get('Topic')]) for x in data]
-        else:
-            return []
+        
+        conn = self.conn()
+
+        c = conn.cursor()
+
+        results = c.execute("""select distinct category, topic from courses where course = ? order by category, topic""", (course,)).fetchall()
+
+        conn.close()
+
+        return results
+
 
     security.declarePublic('getCourseSubtopics')
     def getCourseSubtopics(self, course):
-        data = self.getCourseInfo(course)
-        if data:
-            return [":".join([x.get('Category'), x.get('Topic'), x.get('Subtopic')]) for x in data if x.get('Subtopic')]
-        else:
-            return []
+        
+        conn = self.conn()
 
+        c = conn.cursor()
+
+        results = c.execute("""select distinct category, topic, subtopic from courses where course = ? order by category, topic, subtopic""", (course,)).fetchall()
+
+        conn.close()
+
+        return results
+
+
+    security.declarePublic('getCategories')
+    def getCategories(self):
+        
+        conn = self.conn()
+
+        c = conn.cursor()
+
+        results = c.execute("""select distinct category from courses order by category""").fetchall()
+
+        conn.close()
+
+        return [x[0] for x in results]
+
+
+    security.declarePublic('getTopics')
+    def getTopics(self, category=None):
+        
+        conn = self.conn()
+
+        c = conn.cursor()
+
+        results = c.execute("""select distinct category, topic from courses order by category, topic""").fetchall()
+
+        conn.close()
+        
+        if category:
+            return [x for x in results if x[0] == category]
+        else:
+            return results
+
+
+    security.declarePublic('getSubtopics')
+    def getSubtopics(self, category=None, topic=None):
+        
+        conn = self.conn()
+
+        c = conn.cursor()
+
+        results = c.execute("""select distinct category, topic, subtopic from courses order by category, topic, subtopic""").fetchall()
+
+        conn.close()
+
+        if category and topic:
+            return [x for x in results if (x[0] == category and x[1] == topic and x[2])]
+        elif category:
+            return [x for x in results if x[0] == category]
+        else:
+            return results
 
 
 InitializeClass(ExtensionCourseTool)
