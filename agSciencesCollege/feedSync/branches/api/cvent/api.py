d88c83f549108325a08e017eaac3b8ab1bc50d73
trs22 <trs22@ea9a3244-bef9-0310-a2a5-f2a3d2dd1ec8>
2013-10-09 19:49:32 +0000

    Added Cvent API (rather than HTML screenscraping) option.

---
+++ agSciencesCollege/feedSync/branches/api/cvent/api.py
@@ -0,0 +1,219 @@
+#!/usr/bin/python
+
+# Zope imports
+from zope.component import getSiteManager
+from Products.CMFCore.utils import getToolByName
+from AccessControl.SecurityManagement import newSecurityManager
+from DateTime import DateTime
+
+# My imports
+from time import strptime, strftime
+import urllib2
+import sys
+import re
+
+# SOAP Imports
+from suds.client import Client
+from suds.plugin import MessagePlugin
+from datetime import datetime, timedelta
+
+# API URL
+url = 'https://api.cvent.com/soap/V200611.ASMX?WSDL'
+
+# Define plugin for fixing namespace issues
+class fixNamespace(MessagePlugin):
+    def marshalled(self, context):
+        # Fix Ids namespace in Retrieve
+        for i in context.envelope.childrenAtPath('ns1:Body/ns0:Retrieve/ns0:Ids'):
+            i.setPrefix('ns2')
+
+# Grab event info from Cvent
+def getCventEvents(acct_num='',
+                 login_name='',
+                 passwd='',
+                  start_datestamp='',
+                  end_datestamp=''):
+
+    # Results list
+    results = []
+
+    # Login and set auth header
+    # http://blog.milford.io/2011/11/code-example-using-python-suds-to-access-the-bronto-api/
+
+    client = Client(url, plugins=[fixNamespace()])
+    login = client.service.Login(acct_num, login_name, passwd)
+    session_header = client.factory.create("CventSessionHeader")
+    session_header.CventSessionValue = login._CventSessionHeader
+    client.set_options(soapheaders=session_header)
+
+
+    # Get Updated objects
+
+    _CvObjectType = client.factory.create('ns1:CvObjectType')
+    updated = client.service.GetUpdated(_CvObjectType.Event, start_datestamp, end_datestamp)
+    rv = client.service.Retrieve(_CvObjectType.Event, updated)
+
+
+    # Package object data
+
+    for (t,objects) in rv:
+        for o in objects:
+            if not hasattr(o, '_EventLaunchDate'):
+                # Skip unlaunched Events
+                continue
+
+            # Initialize data
+            r = {}
+    
+            r['id'] = str(o._Id).lower()
+            r['Title'] = str(o._EventTitle)
+            r['location'] = str('%s, %s' % (o._City, o._StateCode))
+            r['zip_code'] = str(o._PostalCode)
+            r['start'] = DateTime(o._EventStartDate)
+            r['end'] = DateTime(o._EventEndDate)
+            r['category'] = str(o._Category)
+
+            for u in o.WeblinkDetail:
+                if u._Target == 'Event Summary':
+                   r['url'] = u._URL
+                   break
+    
+            results.append(r)
+    
+    return results
+
+
+def importEvents(context,
+                 acct_num=acct_num,
+                 login_name=login_name,
+                 passwd=passwd,
+                 emailUsers=['trs22'],
+                 eventsURL="https://agsci.psu.edu/conferences/event-calendar",
+                 owner=None,
+                 daysback=7):
+
+    # Return values
+
+    myStatus = []
+    newEvents = []
+
+
+    # Date calculations
+    end = datetime.utcnow()
+    start = end - timedelta(daysback)
+    
+    start_datestamp = start.strftime('%Y-%m-%dT%H:%M:%S')
+    end_datestamp = end.strftime('%Y-%m-%dT%H:%M:%S')
+
+    # More Zopey goodness
+
+    if owner:
+        admin = context.acl_users.getUserById(owner)
+    else:
+        admin = context.acl_users.getUserById('trs22')
+
+    admin = admin.__of__(context.acl_users)
+    newSecurityManager(None, admin)
+
+    portal = getSiteManager(context)
+    
+    # Extension ZIP Tool
+    zipcode_tool = getToolByName(portal, 'extension_zipcode_tool', None)
+
+    # Extension Course Tool
+    course_tool = getToolByName(portal, 'extension_course_tool', None)
+
+    # Get listing of events, and their cventid if it exists    
+    cventIDs = []
+
+    for myEvent in context.listFolderContents(contentFilter={"portal_type" : "Event"}):
+        cventIDs.append(myEvent.id)
+        myCventID = myEvent.getProperty('cventid')
+        if myCventID:
+            cventIDs.append(myCventID)
+
+
+    cventData = getCventEvents(acct_num=acct_num,
+                               login_name=login_name,
+                               passwd=passwd,
+                               start_datestamp=start_datestamp,
+                               end_datestamp=end_datestamp)
+
+
+    for r in cventData:
+        # Basic Data
+        eventLink = r.get('url')
+        eventId = r.get('id')
+        eventTitle = r.get('Title')
+        eventStart = r.get('start')
+        eventEnd = r.get('end')
+        eventURL = r.get('url')
+        eventLocation = r.get('location')
+        eventZIPCode = r.get('zip_code')
+        
+        # Translations and date manipulation
+
+        eventTitle = eventTitle.decode("utf-8")
+        
+        eventStartDate = eventStart.strftime("%Y-%m-%d")
+        eventStartTime = eventStart.strftime("%H:%M:%S")
+        eventEndDate = eventEnd.strftime("%Y-%m-%d")
+        eventEndTime = eventEnd.strftime("%H:%M:%S")
+
+        if not cventIDs.count(eventId):
+            newEvents.append("<li><a href=\"%s/%s\">%s</a></li>" % (eventsURL, eventId, eventTitle))
+
+            context.invokeFactory(type_name="Event",
+                    id=eventId,
+                    title=eventTitle,
+                    start_date=eventStartDate,
+                    end_date=eventEndDate,
+                    start_time=eventStartTime,
+                    stop_time=eventEndTime,
+                    event_url=eventURL,
+                    location=eventLocation)
+
+            myObject = getattr(context, eventId)
+            myObject.manage_addProperty('cventid', eventId, 'string')
+            myObject.setExcludeFromNav(True)
+            myObject.setLayout("event_redirect_view")
+
+            # Extension-specific fields
+            if zipcode_tool:
+                myObject.zip_code = eventZIPCode
+                zipInfo = zipcode_tool.getZIPInfo(eventZIPCode)
+                if zipInfo:
+                    myObject.extension_counties = (zipInfo[2], )
+
+            if course_tool:
+                course = course_tool.getCourseForEventTitle(eventTitle)
+                if course:
+                    myObject.extension_courses = (course, )
+
+            myObject.reindexObject()
+
+            myStatus.append("Created event %s (id %s)" % (eventTitle, eventId))
+            break
+
+        else:
+            myStatus.append("Skipped event %s (id %s)" % (eventTitle, eventId))
+            #newEvents.append("<li>NOT: <a href=\"%s/%s\">%s</a></li>" % (eventsURL, eventId, eventLink.contents[0]))
+
+    if newEvents:
+        myStatus.append("Sending email to: %s" % ", ".join(emailUsers))
+        mFrom = "do.not.reply@psu.edu"
+        mSubj = "CVENT Events Imported: %s" % portal.getId()
+        mTitle = "<p><strong>The following events from cvent have been imported.</strong></p>"
+        statusText = "\n".join(newEvents)
+        mailHost = context.MailHost
+
+        for myUser in emailUsers:
+            mTo = "%s@psu.edu" % myUser
+
+            mMsg = "\n".join(["\n\n", mTitle, "<ul>", statusText, "<ul>"])
+            mailHost.secureSend(mMsg.encode('utf-8'), mto=mTo, mfrom=mFrom, subject=mSubj, subtype='html')
+
+    #transaction.commit()
+    myStatus.append("Finished Loading")
+    return "\n".join(myStatus)
+    #return newEvents
